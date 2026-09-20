f9a1e1bb3903b0d7bf6bf6ef1e0841071995161b
Ayan Khan <AyanKhanOffiiciall@gmail.com>
2026-09-21 05:28:56 +0530

    fix: correct newline handling in input cleanup

--- mini-projects/student-grade-manager/student_grade_manager.c
+++ mini-projects/student-grade-manager/student_grade_manager.c
@@ -30,7 +30,7 @@ void displayClassStatistics(const Student students[], int count);
 static void clearInputLine(void) {
     int ch;
 
-    while ((ch = getchar()) != '\\n' && ch != EOF) {
+    while ((ch = getchar()) != '\n' && ch != EOF) {
     }
 }
 
